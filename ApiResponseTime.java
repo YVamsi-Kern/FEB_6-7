b6f33e3f298758ad78576a1624664fa2eaf450e5
YVamsi-Kern <vamsi@kernsoftwaresolutions.com>
2026-02-09 14:04:49 +0530

    Add files via upload

---
+++ ApiResponseTime.java
@@ -0,0 +1,18 @@
+public class ApiResponseTime {
+    public static void main(String[] args) {
+        int[] responseTimes = {500, 1500, 2500, 1800, 3000};
+
+        int sum = 0;
+
+        System.out.println("Slow APIs:");
+        for (int time : responseTimes) {
+            if (time > 2000) {
+                System.out.println(time + " ms");
+            }
+            sum += time;
+        }
+
+        double average = (double) sum / responseTimes.length;
+        System.out.println("Average Response Time: " + average + " ms");
+    }
+}
